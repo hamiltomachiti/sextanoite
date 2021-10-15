0ed54bca29d7ac475a8548d2c4231df093cd5a56
hamiltomachiti <hamiltomachiti@yahoo.com.br>
2021-10-14 21:31:02 -0300

    Implementação da cliente_service

--- angular-cli/Projeto-Cliente/src/app/clientes/cliente-inserir/cliente-inserir.component.ts
+++ angular-cli/Projeto-Cliente/src/app/clientes/cliente-inserir/cliente-inserir.component.ts
@@ -1,6 +1,6 @@
 import { Component,EventEmitter, Output} from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { Cliente } from '../cliente.model';
+import { ClienteService } from "../cliente.service";
 
 @Component({
   selector: 'app-cliente-inserir',
@@ -8,30 +8,21 @@ import { Cliente } from '../cliente.model';
   styleUrls:['./cliente-inserir.component.css'],
 })
 
-
-
 export class ClienteInserirComponent{
-  @Output() clienteAdicionado = new EventEmitter<Cliente>();
-  nome:string;
-  fone: string;
-  email: string;
 
-  constructor(){
-    this.nome = "";
-    this.fone = "";
-    this.email = "";
-  }
+  constructor(public clienteService:ClienteService){}
+
 
   onAdicionarCliente(form: NgForm){
     if(form.invalid){
       return;
     }
-    const cliente: Cliente = {
-      nome: form.value.nome,
-      fone: form.value.fone,
-      email: form.value.email,
-      };
-      this.clienteAdicionado.emit(cliente);
+    this.clienteService.adicionarCliente(
+      form.value.nome,
+      form.value.fone,
+      form.value.email
+    );
+    form.resetForm();
   }
 }
 
--- angular-cli/Projeto-Cliente/src/app/clientes/cliente-lista/cliente-lista.component.ts
+++ angular-cli/Projeto-Cliente/src/app/clientes/cliente-lista/cliente-lista.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { Cliente } from '../cliente.model';
 import { ClienteService } from '../cliente.service';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cliente-lista',
   templateUrl: './cliente-lista.component.html',
   styleUrls: ['./cliente-lista.component.css']
 })
-export class ClienteListaComponent implements OnInit {
-  @Input() clientes: Cliente[] = [];
+export class ClienteListaComponent implements OnInit, OnDestroy {
+  clientes: Cliente[] = [];
+  private clientesSubscription: Subscription = new Subscription;
 
 
   constructor(public clienteService: ClienteService) { }
 
   ngOnInit(): void {
+    this.clientes = this.clienteService.getClientes();
+    this.clientesSubscription = this.clienteService
+    .getListaDeClientesAtualizadaObservable()
+    .subscribe((clientes:Cliente[])=>{
+      this.clientes = clientes;
+    });
+  }
+
+  ngOnDestroy():void{
+    this.clientesSubscription.unsubscribe();
   }
 
 }
